50875753ff982ab6c40aaf826a0c630d399d31bc
Francis Lahoud <flahoud@student.42quebec.com>
2024-01-27 02:00:13 -0500

    Create format_ft_time.py

---
+++ 0-starting/ex01/format_ft_time.py
@@ -0,0 +1,11 @@
+import datetime as dt
+
+today = dt.date.today()
+
+diff = dt.datetime.now() - dt.datetime(1970,1,1)
+
+diff_float = '{:,.4f}'.format(diff.total_seconds())
+diff_sci = '{:.2e}'.format(diff.total_seconds())
+print(f"Seconds since January 1, 1970: {diff_float} or {diff_sci} in scientific notation$")
+
+print(today.strftime("%B %d %Y"))
