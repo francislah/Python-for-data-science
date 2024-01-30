2ea8ed76df1cbd186ed5a69df60f15a4ca0036eb
Francis Lahoud <flahoud@student.42quebec.com>
2024-01-30 12:12:34 -0500

    Update Loading.py

--- 0-starting/ex08/Loading.py
+++ 0-starting/ex08/Loading.py
@@ -1,11 +1,15 @@
+# import shutil
+
 def ft_tqdm(lst: range) -> None:
     percent = 0
     total = len(lst)
-    spaces = " " * 121
-    for i, x in enumerate(lst):
-        yield x
+    # term_length = shutil.get_terminal_size().columns - 46 + len(str(total) * 2)
+    term_length = 100
+    spaces = " " * term_length
+    for i in lst:
+        yield i
         percent = '{:3.0f}'.format(i / total * 100)
-        loading = spaces.replace(' ', '█', round(i / total * len(spaces)))
+        loading = spaces.replace(' ', '█', round(i / total * term_length))
         print(f"{percent}%|{loading}| {i + 1:>{len(str(total))}}/{total}", end='\r')
 
 
