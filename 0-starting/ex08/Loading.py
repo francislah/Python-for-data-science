cc5068710fbe67fb90b27fc14d19d0ac8ece142c
Francis Lahoud <flahoud@student.42quebec.com>
2024-03-03 00:07:26 -0500

    apply norminette

--- 0-starting/ex08/Loading.py
+++ 0-starting/ex08/Loading.py
@@ -3,14 +3,15 @@
 def ft_tqdm(lst: range) -> None:
     percent = 0
     total = len(lst)
-    # term_length = shutil.get_terminal_size().columns - 46 + len(str(total) * 2)
+# term_length = shutil.get_terminal_size().columns - 46 + len(str(total) * 2)
     term_length = 100
     spaces = " " * term_length
     for i in lst:
         yield i
         percent = '{:3.0f}'.format(i / total * 100)
         loading = spaces.replace(' ', '█', round(i / total * term_length))
-        print(f"{percent}%|{loading}| {i + 1:>{len(str(total))}}/{total}", end='\r')
+        print(f"{percent}%|{loading}| \
+              {i + 1:>{len(str(total))}}/{total}", end='\r')
 
 
 ft_tqdm.__doc__ = \
